7825a490a99c701fadc5dcf01446b3fbab8d6c00
kimsak <saksak.kim@gmail.com>
2013-08-12 16:16:19 +0900

    コンポーネントクラスの追加

---
+++ lib/Component.h
@@ -0,0 +1,18 @@
+//
+//  Component.h
+//  KandataGame
+//
+//  Created by 木村 裕作 on 2013/08/10.
+//  Copyright (c) 2013年 木村 裕作. All rights reserved.
+//
+
+#ifndef COMPONENT_H_
+#define COMPONENT_H_
+
+class IComponent {
+public:
+    // 仮想デストラクタ
+    virtual ~IComponent() {}
+};
+
+#endif
